81e0ae05d3f914a54fd82d26bcc2d39a8cbede03
Simon Mölzer <simon.molzer@industrilas.com>
2023-02-07 13:42:03 +0100

    Change lock screen command to use png instead of jpg for better quality

--- dotfiles/scripts/lockscreen.sh
+++ dotfiles/scripts/lockscreen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-scrot -o -f /tmp/screendump.jpg
-convert /tmp/screendump.jpg -blur 0x4 /tmp/lockscreen.jpg
-feh --fullscreen -g 4000x2560 --no-xinerama -s /tmp/lockscreen.jpg &
+scrot -o -f /tmp/screendump.png
+convert /tmp/screendump.png -blur 0x4 /tmp/lockscreen.png
+feh --fullscreen -g 4000x2560 --no-xinerama -s /tmp/lockscreen.png &
 FEHPID=$!
 xtrlock
 kill -15 $FEHPID
