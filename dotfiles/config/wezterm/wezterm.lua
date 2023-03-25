bc753617f456299e854e4700ba5e6934f7943699
Simon Mölzer <evilhowser@gmail.com>
2023-03-25 23:38:16 +0100

    Add wezterm config

---
+++ dotfiles/config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+
+local config= {
+
+}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.font = wezterm.font('Noto Sans Mono')
+config.freetype_load_target = "Light"
+config.freetype_load_flags = 'FORCE_AUTOHINT'
+config.font_size = 11
+config.enable_tab_bar = false
+
+return config
