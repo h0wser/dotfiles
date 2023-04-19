1045f09cfd7510b1e86cf377d723e64ccba40534
Simon Mölzer <simon.molzer@industrilas.com>
2023-04-19 11:33:04 +0200

    Changed wezterm font

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -8,10 +8,11 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Gruvbox dark, medium (base16)'
-config.font = wezterm.font('CaskaydiaCove Nerd Font Mono')
-config.freetype_load_target = "Light"
-config.freetype_load_flags = 'FORCE_AUTOHINT'
+--config.color_scheme = 'Gruvbox dark, medium (base16)'
+config.color_scheme = 'Everforest Dark (Gogh)'
+config.font = wezterm.font('MesloLGS Nerd Font Mono')
+--config.freetype_load_target = "Light"
+--config.freetype_load_flags = 'FORCE_AUTOHINT'
 config.font_size = 11
 config.enable_tab_bar = false
 
