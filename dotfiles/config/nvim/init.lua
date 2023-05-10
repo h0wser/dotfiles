908969f8f2cbe3f037ff9bb0350aa4b74144002c
Simon Mölzer <simon.molzer@industrilas.com>
2023-05-10 11:13:45 +0200

    Fix luasnip jumping to snippets halfway across the file

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -12,6 +12,12 @@ local luasnip = require('luasnip')
 
 local select_opts = { behaviour = cmp.SelectBehavior.Select}
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -34,16 +40,14 @@ cmp.setup({
 	mapping = {
 		['<CR>'] = cmp.mapping.confirm({select = false}),
 		['<Tab>'] = cmp.mapping(function(fallback)
-			local col = vim.fn.col('.') - 1
 			if cmp.visible() then
 				cmp.select_next_item(select_opts)
-			elseif luasnip.jumpable(1) then
-				luasnip.jump(1)
-			elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-				fallback()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
 			else
 				fallback()
-				--cmp.complete()
 			end
 		end, {'i', 's'}),
 		['<S-Tab>'] = cmp.mapping(function(fallback)
